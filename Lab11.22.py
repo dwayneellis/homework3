ae5b1191f99bb99d0555f36c777aa817905b7946
dwayneellis <70253282+dwayneellis@users.noreply.github.com>
2020-10-31 11:48:28 -0500

    Add files via upload

---
+++ Lab11.22.py
@@ -0,0 +1,16 @@
+# CIS 2348 Homework 3 Fall 2020.
+# Dwayne Ellis
+# Student ID: 0833810
+# Lab 11.22 Word frequencies
+# Write a program that reads a list of words. Then, the program outputs those words and their frequencies.
+
+# Read input from user and split word list by space and store as a list
+words = input()
+
+Word_List = words.split()
+
+# Loop runs till the length of word list
+for i in range(len(Word_List)):
+
+    # Printing output with word and count
+    print(Word_List[i], Word_List.count(Word_List[i]))
